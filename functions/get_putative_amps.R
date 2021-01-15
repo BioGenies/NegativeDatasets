b9cc7bf1a3e02daec0179cde3d9a611eae0fc8e6
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2021-01-15 10:44:28 +0100

    added putative amps and exemplary positive dataset

---
+++ functions/get_putative_amps.R
@@ -0,0 +1,10 @@
+get_putative_amps <- function(data_path, keywords = c("Antimicrobial", "Antibacterial", "Antiviral", "Fungicide",
+                                                      "defense", "Defensin", "Bacteriocin")) {
+  seqs <- read_fasta(paste0(data_path, "Data/uniprot-reviewed yes.fasta")) 
+  annot <- read.delim(paste0(data_path, "Data/uniprot-reviewed yes.tab"))
+  new_seq_names <- sapply(names(seqs), function(x) strsplit(x, "|", fixed = TRUE)[[1]][2])
+  names(seqs) <- new_seq_names
+  
+  putative_amps <- filter_by_annotations(seqs, annot, keywords_vec = keywords, exclude = FALSE) 
+  save(putative_amps, file = "data/putative_AMPs.rda")
+}
