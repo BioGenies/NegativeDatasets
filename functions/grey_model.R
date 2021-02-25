ddde011b6e2de94b7d25fe747d771756fd388bd6
Laura <erdaradun.gaztea@gmail.com>
2021-02-25 08:44:56 +0100

    devectorized grey model, because sequences can have different lengths

--- functions/grey_model.R
+++ functions/grey_model.R
@@ -1,35 +1,34 @@
 #' Create grey model GM(1, 1)
 #' 
 #' @description Builds grey model with parameters 1 and 1
-#' from the supplied data. Returns a matrix with \code{a}
-#' and \code{b} coefficients for each observation.
+#' from the supplied data. Returns a numeric vector of
+#' length 2 with \code{a} and \code{b} coefficients for 
+#' supplied observation.
 #' 
-#' @param data
-#'  Training data in numeric matrix format without target
-#'  column. Each row contains data about one sequence.
+#' @param row
+#'  Training data in numeric vector format without target
+#'  value. Should contain data about one sequence.
 #' 
 #' @examples
-#' X <- matrix(1:15, nrow = 5,
-#'             dimnames = list(LETTERS[11:15], c("V1", "V2", "V3")))
-#' grey_model_1_1(X)
+#' X <- list(
+#'   X1 = 1:5,
+#'   X2 = c(10, 12, 24, 60),
+#'   X3 = 9:2
+#' )
+#' lapply(X, grey_model_1_1)
 #' 
 #' @noRd
-grey_model_1_1 <- function(data) {
+grey_model_1_1 <- function(row) {
   # First we compute first-order AGO
-  ago <- t(apply(data, 1, cumsum))
+  ago <- cumsum(row)
   # Unfortunately, had to write for-loop, because of trying to
   #  iterate over rows of two matrices simultaneously.
   # -a is called "developing coefficient"
   # b is called "influence coefficient"
   # [a, b]^T = [B^T*B]^-1*B^T*Y
-  ret <- matrix(nrow = nrow(data), ncol = 2,
-                dimnames = list(rownames(data), c("a", "b")))
-  for (i in 1:nrow(data)) {
-    B <- cbind(-0.5*(ago[i, -ncol(ago)] + ago[i, -1]),
-               1)
-    Y <- data[i, -1]
-    # ^ has higher precedence than %*%, so no parentheses necessary
-    ret[i, ] <- (t(B) %*% B)^-1 %*% t(B) %*% Y
-  }
-  ret
+  B <- cbind(-0.5*(ago[-length(ago)] + ago[-1]), 1)
+  Y <- row[-1]
+  # ^ has higher precedence than %*%, so no parentheses necessary
+  ret <- as.numeric((t(B) %*% B)^-1 %*% t(B) %*% Y)
+  setNames(ret, c("a", "b"))
 }
