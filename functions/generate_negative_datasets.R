5b7a53f2857b4b0ff116a7931bfb4c6e2f181d47
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2021-03-08 19:00:57 +0100

    updated functions for sampling negative datasets

--- functions/generate_negative_datasets.R
+++ functions/generate_negative_datasets.R
@@ -1,21 +1,66 @@
+# MLAMP
+generate_negative_dataset_1 <- function(sequences, uniprot_data) {
+  seqs <- filter_with_cdhit(
+    filter_nonstandard_aa(
+      filter_by_annotations(
+        filter_by_lengths(sequences, 5, 100),
+        uniprot_data, 
+        c("Antimicrobial", "Antibiotic", "Fungicide", "Defensin"))),
+    0.4)
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=1_AMP=0")
+  seqs
+}
+
+
+# ampir (precursor)
+generate_negative_dataset_2 <- function(sequences, positive_dataset) {
+  seqs <- filter_random_sequences(
+    filter_by_lengths(
+      filter_nonstandard_aa(
+        filter_out_positive_sequences(
+          filter_with_cdhit(sequences, 0.9),
+          positive_dataset)),
+      51, 500),
+    10*length(positive_dataset))
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=2_AMP=0")
+  seqs
+}
+
+
+# ampir (mature)
+generate_negative_dataset_3 <- function(sequences, positive_dataset) {
+  seqs <- filter_nonstandard_aa(
+    filter_by_lengths(
+      filter_out_positive_sequences(
+        filter_with_cdhit(sequences, 0.9),
+        positive_dataset),
+      11, 39))
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=3_AMP=0")
+  seqs
+}
+
+
 # CS-AMPPred
-generate_negative_dataset_1 <- function(sequences, uniprot_data, positive_dataset) {
-  filter_random_sequences(
+generate_negative_dataset_4 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- filter_random_sequences(
     filter_with_cdhit(
-      filter_by_lengths(
-        filter_by_annotations(sequences = sequences, 
-                              uniprot_data = uniprot_data, 
-                              keywords_vec = "Antimicrobial", 
-                              exclude = TRUE),
-        16, 90),
+      filter_nonstandard_aa(
+        filter_by_lengths(
+          filter_by_annotations(sequences = sequences, 
+                                uniprot_data = uniprot_data, 
+                                keywords_vec = "Antimicrobial", 
+                                exclude = TRUE),
+          16, 90)),
       0.4),
     length(positive_dataset))
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=4_AMP=0")
+  seqs
 }
 
 
 # SVM-LZ 2nd dataset
-generate_negative_dataset_2 <- function(sequences, uniprot_data, positive_dataset) {
-  filter_with_cdhit(
+generate_negative_dataset_5 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- filter_with_cdhit(
     filter_nonstandard_aa(
       generate_cutted_sequences(
         filter_random_sequences(
@@ -26,91 +71,111 @@ generate_negative_dataset_2 <- function(sequences, uniprot_data, positive_datase
           10000),
         sample(lengths(positive_dataset), 10000, replace = TRUE))), 
     0.7)
-}
-
-
-# MLAMP
-generate_negative_dataset_3 <- function(sequences, uniprot_data) {
-  filter_with_cdhit(
-    filter_nonstandard_aa(
-      filter_by_annotations(
-        filter_by_lengths(sequences, 5, 100),
-        uniprot_data, 
-        c("Antimicrobial", "Antibiotic", "Fungicide", "Defensin"))),
-    0.4)
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=5_AMP=0")
+  seqs
 }
 
 
 # AmpGram
-generate_negative_dataset_4 <- function(sequences, uniprot_data, positive_dataset) {
-  generate_cutted_sequences(
+generate_negative_dataset_6 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- generate_cutted_sequences(
     filter_nonstandard_aa(
       filter_by_annotations(sequences = sequences, 
                             uniprot_data = uniprot_data, 
                             keywords_vec = c("Antimicrobial", "Transit peptide", "Antibacterial", "Antiviral", "Fungicide", "Secreted"), 
                             exclude = TRUE)),
     lengths(positive_dataset))
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=6_AMP=0")
+  seqs
 }
 
 
 # dbAMP
-generate_negative_dataset_5 <- function(sequences, uniprot_data, positive_dataset) {
-  filter_with_cdhit(
-    filter_by_lengths(
-      filter_by_annotations(sequences = sequences, 
-                            uniprot_data = uniprot_data, 
-                            keywords_vec = c("Antimicrobial", "Antibiotic", "Antiviral", "Fungicide", "Secreted", "Defensin", "Toxin", "Transmembrane"), 
-                            exclude = TRUE),
-      10, 100),
+generate_negative_dataset_7 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- filter_with_cdhit(
+    filter_nonstandard_aa(
+      filter_by_lengths(
+        filter_by_annotations(sequences = sequences, 
+                              uniprot_data = uniprot_data, 
+                              keywords_vec = c("Antimicrobial", "Antibiotic", "Antiviral", "Fungicide", "Secreted", "Defensin", "Toxin", "Transmembrane"), 
+                              exclude = TRUE),
+        10, 100)),
     0.4)
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=7_AMP=0")
+  seqs
 }
 
 
 # Deep learning regression model for antimicrobial peptide design 
-generate_negative_dataset_6 <- function(sequences, uniprot_data, positive_dataset) {
-  generate_cutted_sequences(
+generate_negative_dataset_8 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- generate_cutted_sequences(
     filter_with_cdhit(
-      filter_by_location(
-        filter_by_annotations(sequences = sequences, 
-                              uniprot_data = uniprot_data, 
-                              keywords_vec = c("Antimicrobial", "Antibiotic", "Antiviral", "Fungicide", "Secreted"), 
-                              exclude = TRUE),
-        uniprot_data, 
-        location = "Cytoplasm", 
-        evidence = "ECO:0000269"),
+      filter_nonstandard_aa(
+        filter_by_location(
+          filter_by_annotations(sequences = sequences, 
+                                uniprot_data = uniprot_data, 
+                                keywords_vec = c("Antimicrobial", "Antibiotic", "Antiviral", "Fungicide", "Secreted"), 
+                                exclude = TRUE),
+          uniprot_data, 
+          location = "Cytoplasm", 
+          evidence = "ECO:0000269")),
       0.4),
     lengths(positive_dataset), 
     excluded_aa = "C")
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=8_AMP=0")
+  seqs
 }
 
 
-# ampir (precursor)
-generate_negative_dataset_7 <- function(sequences, positive_dataset) {
-  filter_random_sequences(
-    filter_by_lengths(
+# ACEP/AMPScanner V2
+generate_negative_dataset_9 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- generate_cutted_sequences(
+    filter_with_blat(
       filter_nonstandard_aa(
-        filter_out_positive_sequences(
-          filter_with_cdhit(sequences, 0.9),
-          positive_dataset)),
-      51, 500),
-    10*length(positive_dataset))
+        filter_by_lengths(
+          filter_by_annotations(
+            filter_by_location(sequences, uniprot_data, "Cytoplasm"),
+            uniprot_dat,
+            c("Antimicrobial", "Antiviral", "Antibiotic", "Fungicide", "Secreted")),
+          10, max(lengths(sequences)))),
+      positive_dataset),
+    lengths(positive_dataset))
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=9_AMP=0")
+  seqs
 }
 
 
-# ampir (mature)
-generate_negative_dataset_8 <- function(sequences, positive_dataset) {
-  filter_nonstandard_aa(
-    filter_by_lengths(
-      filter_out_positive_sequences(
-        filter_with_cdhit(sequences, 0.9),
-        positive_dataset),
-      11, 39))
+# Gabere&Noble
+generate_negative_dataset_10 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- generate_cutted_sequences(
+    filter_nonstandard_aa(
+      filter_by_location(
+        filter_by_annotations(sequences = sequences,
+                              uniprot_data = uniprot_data,
+                              keywords_vec = "Antimicrobial",
+                              exclude = TRUE),
+        location = "Cytoplasm|Endoplasmic reticulum|Mitochondrion|Golgi")),
+    6*lengths(positive_dataset))
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=10_AMP=0")
+  seqs
+}
+
+
+# AMAP
+generate_negative_dataset_11 <- function(sequences, uniprot_data, positive_dataset) {
+  seqs <- filter_with_cdhit(
+    generate_negative_dataset_10(sequences = sequences,
+                                 uniprot_data = uniprot_data,
+                                 positive_dataset = positive_dataset),
+    0.4)
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=11_AMP=0")
+  seqs
 }
 
 
 # AMPlify
-generate_negative_dataset_9 <- function(sequences, uniprot_data, positive_dataset, potential_AMPs) {
-  generate_cutted_sequences(
+generate_negative_dataset_12 <- function(sequences, uniprot_data, positive_dataset, potential_AMPs) {
+  seqs <- generate_cutted_sequences(
     filter_out_positive_sequences(
       filter_nonstandard_aa(
         filter_by_annotations(
@@ -119,42 +184,6 @@ generate_negative_dataset_9 <- function(sequences, uniprot_data, positive_datase
           c("Antimicrobial", "Antibiotic", "defense", "Defensin", "Bacteriocin", "Fungicide"))), 
       c(positive_dataset, potential_AMPs)),
     lengths(positive_dataset))
-}
-
-
-# ACEP/AMPScanner V2
-generate_negative_dataset_10 <- function(sequences, uniprot_data, positive_dataset) {
-  generate_cutted_sequences(
-    filter_with_blat(
-      filter_by_lengths(
-        filter_by_annotations(
-          filter_by_location(sequences, uniprot_data, "Cytoplasm"),
-          uniprot_dat,
-          c("Antimicrobial", "Antiviral", "Antibiotic", "Fungicide", "Secreted")),
-        10, max(lengths(sequences))),
-      positive_dataset),
-    lengths(positive_dataset))
-}
-
-
-# Gabere&Noble
-generate_negative_dataset_11 <- function(sequences, uniprot_data, positive_dataset) {
-  generate_cutted_sequences(
-    filter_by_location(
-      filter_by_annotations(sequences = sequences,
-                            uniprot_data = uniprot_data,
-                            keywords_vec = "Antimicrobial",
-                            exclude = TRUE),
-      location = "Cytoplasm|Endoplasmic reticulum|Mitochondrion|Golgi"),
-    6*lengths(positive_dataset))
-}
-
-
-# AMAP
-generate_negative_dataset_12 <- function(sequences, uniprot_data, positive_dataset) {
-  filter_with_cdhit(
-    generate_negative_dataset_11(sequences = sequences,
-                                 uniprot_data = uniprot_data,
-                                 positive_dataset = positive_dataset),
-    0.4)
+  names(seqs) <- paste0("Seq", 1:length(seqs), "_sampling_method=12_AMP=0")
+  seqs
 }
