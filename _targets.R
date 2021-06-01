9ceea99d3647f4a1a439014fdc556bee1a30c536
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2021-06-01 10:23:13 +0200

    moved plot data generation to targets pipeline

--- _targets.R
+++ _targets.R
@@ -18,6 +18,7 @@ source("functions/generate_holdouts.R")
 source("functions/generate_negative_datasets.R")
 source("functions/generate_all_traintest_datasets.R")
 source("functions/get_putative_amps.R")
+source("functions/plot_functions.R")
 
 
 list(
@@ -60,6 +61,80 @@ list(
                                     positive_traintest = positive_traintest,
                                     putative_amps = putative_amps, 
                                     positive_benchmark = positive_benchmark)
+  ),
+  tar_target(
+    methods,
+    c("AMAP", "AmPEP", "AmpGram", "ampir-mature", "ampir-precursor", "AMPlify",
+      "AMPScannerV2", "CSAMPPred", "dbAMP", "GabereNoble", "iAMP2L", "Wang", "Witten")
+  ),
+  tar_target(
+    dataset_colors,
+    c(Positive = "#ff4242", AMAP = "#f2b176", AmPEP = "#b4b4b4", AmpGram = "#f27676", `ampir-mature` = "#76bef2",
+      `ampir-precursor` = "#76eff2", AMPlify = "#f2d676", AMPScannerV2 = "#b976f2", CSAMPPred = "#76f2be", 
+      dbAMP = "#f276e8", GabereNoble = "#7688f2", iAMP2L = "#eff275", Wang = "#80f276", Witten = "#ccf276")
+  ),
+  tar_target(
+    aa_comp_peptides_with_names,
+    calculate_aa_comp_peptides(methods, 5, data_path)
+  ),
+  tar_target(
+    aa_comp_peptides,
+    select(aa_comp_peptides_with_names, -prot)
+  ),
+  tar_target(
+    aa_comp_peptides_positive,
+    get_aa_comp_positive(positive_traintest)
+  ),
+  tar_target(
+    aa_comp_peptides_all,
+    bind_rows(aa_comp_peptides,
+              aa_comp_peptides_pos) %>% 
+      mutate(method = factor(method, levels = c(methods, "Positive")))
+  ),
+  tar_target(
+    aa_comp,
+    calculate_aa_comp_datasets(methods, 5, data_path)
+  ),
+  tar_target(
+    aa_comp_pos,
+    get_aa_comp_pos(positive_traintest)
+  ),
+  tar_target(
+    aa_comp_all,
+    bind_rows(mutate(aa_comp, 
+                     rep = as.character(rep),
+                     Dataset = "Negative"), 
+              mutate(aa_comp_pos,
+                     Dataset = "Positive"))
+  ),
+  tar_target(
+    df_neg,
+    get_prop_df(methods, 5, data_path)
+  ),
+  tar_target(
+    df_pos,
+    calculate_properties(positive_traintest, "Positive", "1")
+  ),
+  tar_target(
+    df_all,
+    bind_rows(mutate(df_pos, Dataset = "Positive"),
+              mutate(df_neg, Dataset = "Negative",
+                     rep = as.character(rep))) %>% 
+      mutate(method = factor(method, levels = names(dataset_colors)))
+  ),
+  tar_target(
+    ngram_counts_sum,
+    get_ngram_counts_sum(methods, 5, data_path)
+  ),
+  tar_target(
+    ngram_counts_sum_pos,
+    get_ngram_counts_sum_pos(positive_traintest)
+  ),
+  tar_target(
+    ngram_counts_sum_all,
+    bind_rows(mutate(ngram_counts_sum, Dataset = "Negative"),
+              mutate(ngram_counts_sum_pos, Dataset = "Positive")) %>% 
+      mutate(method = factor(method, levels = names(dataset_colors))) 
   )
 )
 
--- functions/plot_functions.R
+++ functions/plot_functions.R
@@ -61,8 +61,62 @@ calculate_aa_comp_peptides <- function(methods, n_rep, data_path) {
                                                             "R", "S", "T", "V", "W", "Y")))/length(neg[[ith_prot]])) %>%
           setNames(c("Amino acid", "Frequency"))  %>%
           mutate(method = i,
-                 rep = j)
+                 rep = j,
+                 prot = names(neg))
       }) %>% bind_rows()
     }) %>% bind_rows()
   }) %>% bind_rows()
 } 
+
+get_aa_comp_peptides_positive <- function(positive) {
+  lapply(names(positive), function(ith_prot) {
+    data.frame(table(factor(positive[[ith_prot]], levels = c("A", "C", "D", "E", "F", "G", "H",
+                                                             "I", "K", "L", "M", "N", "P", "Q",
+                                                             "R", "S", "T", "V", "W", "Y")))/length(positive[[ith_prot]])) %>%
+      setNames(c("Amino acid", "Frequency"))  %>%
+      mutate(method = "Positive",
+             rep = 1)
+  }) %>% bind_rows()
+}
+
+get_aa_comp_pos <- function(positive) {
+  aa_pos <- unlist(positive, use.names = FALSE)
+  as.data.frame(table(aa_pos)/length(aa_pos)) %>% 
+    setNames(c("aa", "Freq")) %>% 
+    mutate(method = "Positive",
+           rep = "1")
+}
+
+get_ngram_counts_sum <- function(methods, n_rep, data_path) {
+  lapply(methods, function(i) {
+    lapply(1:n_rep, function(j) {
+      ds <- read_fasta(paste0(data_path, "Training_method_", i, "_rep", j, ".fasta"))
+      seqs <- ds[which(grepl("AMP=0", names(ds)))]
+      ngrams <- count_multimers(seqs,
+                                k_vector = c(rep(2, 4), rep(3, 4)),
+                                kmer_gaps_list = list(NULL, 1, 2, 3, c(0, 0), c(1, 0), c(0, 1), c(1, 1)),
+                                alphabet = toupper(colnames(biogram::aaprop))) %>%
+        as.matrix() %>% 
+        colSums()/length(seqs) 
+      ngrams %>% 
+        t() %>% 
+        as.data.frame() %>% 
+        mutate(method = i,
+               rep = j)
+    }) %>% bind_rows()
+  }) %>% bind_rows()
+}
+
+get_ngram_counts_pos <- function(positive) {
+  count_multimers(positive,
+                  k_vector = c(rep(2, 4), rep(3, 4)),
+                  kmer_gaps_list = list(NULL, 1, 2, 3, c(0, 0), c(1, 0), c(0, 1), c(1, 1)),
+                  alphabet = toupper(colnames(biogram::aaprop))) %>%
+    as.matrix() %>% 
+    colSums()/length(positive) 
+  ngram_counts_sum_pos <- ngram_counts_sum_pos %>% 
+    t() %>% 
+    as.data.frame() %>% 
+    mutate(method = "Positive",
+           rep = 1)
+}
