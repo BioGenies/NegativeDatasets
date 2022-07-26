ead2410d88b5f94b421e730dc53ad664c0beea52
michbur <michalburdukiewicz@gmail.com>
2022-07-26 17:58:11 +0200

    added rmd scripts

---
+++ docs/rmd_scripts.R
@@ -0,0 +1,12 @@
+negative_sampling_citation <- function() {
+  "
+  ## Citation
+  
+  Katarzyna Sidorczuk, Przemysław Gagat, Filip Pietluch, Jakub Kała, Dominik Rafacz, 
+  Laura Bąkała, Jadwiga Słowik, Rafał Kolenda, Stefan Rödiger, Legana C H W Fingerhut, 
+  Ira R Cooke, Paweł Mackiewicz, Michał Burdukiewicz [*Benchmarks in antimicrobial 
+  peptide prediction are biased due to the selection of negative 
+  data.*](https://doi.org/10.1101/2022.05.30.493946)
+  
+  "
+}
