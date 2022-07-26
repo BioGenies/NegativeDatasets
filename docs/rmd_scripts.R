d29f931c3eec8d73a8965f2829a318da6226bba9
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2022-07-26 20:09:14 +0100

    updated scripts

--- docs/rmd_scripts.R
+++ docs/rmd_scripts.R
@@ -1,7 +1,7 @@
 negative_sampling_citation <- function() {
   "
   
-  # Citation
+# Citation
   
   Katarzyna Sidorczuk, Przemysław Gagat, Filip Pietluch, Jakub Kała, Dominik Rafacz, 
   Laura Bąkała, Jadwiga Słowik, Rafał Kolenda, Stefan Rödiger, Legana C H W Fingerhut, 
@@ -9,5 +9,28 @@ negative_sampling_citation <- function() {
   peptide prediction are biased due to the selection of negative 
   data.*](https://doi.org/10.1101/2022.05.30.493946)
   
+  "
+}
+
+negative_sampling_links <- function() {
+  "
+  
+# Important links
+
+ - https://github.com/BioGenies/NegativeDatasets: the repository containing the code necessary to reproduce results of our analysis.
+ - https://github.com/BioGenies/NegativeDatasetsArchitectures: the repository containing all architectures considered in our analysis.
+ - https://github.com/BioGenies/AMPBenchmark: the source code of AMPBenchmark.
+  
+  "
+}
+
+negative_sampling_contact <- function() {
+  "
+  
+# Contact
+
+If you have any questions, suggestions or comments, contact [Michal Burdukiewicz](mailto:michalburdukiewicz@gmail.com).
+ 
+ 
   "
 }
