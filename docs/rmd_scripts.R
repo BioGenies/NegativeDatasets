b421d2bd8f4403377df75248dd89a23ef79afcd8
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2022-07-26 20:24:50 +0100

    updated scripts

--- docs/rmd_scripts.R
+++ docs/rmd_scripts.R
@@ -1,7 +1,7 @@
 negative_sampling_citation <- function() {
   "
   
-# Citation
+## Citation
   
   Katarzyna Sidorczuk, Przemysław Gagat, Filip Pietluch, Jakub Kała, Dominik Rafacz, 
   Laura Bąkała, Jadwiga Słowik, Rafał Kolenda, Stefan Rödiger, Legana C H W Fingerhut, 
@@ -15,7 +15,7 @@ negative_sampling_citation <- function() {
 negative_sampling_links <- function() {
   "
   
-# Important links
+## Important links
 
  - https://github.com/BioGenies/NegativeDatasets: the repository containing the code necessary to reproduce results of our analysis.
  - https://github.com/BioGenies/NegativeDatasetsArchitectures: the repository containing all architectures considered in our analysis.
@@ -27,7 +27,7 @@ negative_sampling_links <- function() {
 negative_sampling_contact <- function() {
   "
   
-# Contact
+## Contact
 
 If you have any questions, suggestions or comments, contact [Michal Burdukiewicz](mailto:michalburdukiewicz@gmail.com).
  
