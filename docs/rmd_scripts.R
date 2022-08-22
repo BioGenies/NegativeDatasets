8d14fe1c560f2c2f14064379c9d90615e10859df
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2022-08-22 17:01:51 +0200

    updated citation

--- docs/rmd_scripts.R
+++ docs/rmd_scripts.R
@@ -3,11 +3,10 @@ negative_sampling_citation <- function() {
   
 ## Citation
   
-  Katarzyna Sidorczuk, Przemysław Gagat, Filip Pietluch, Jakub Kała, Dominik Rafacz, 
-  Laura Bąkała, Jadwiga Słowik, Rafał Kolenda, Stefan Rödiger, Legana C H W Fingerhut, 
-  Ira R Cooke, Paweł Mackiewicz, Michał Burdukiewicz [*Benchmarks in antimicrobial 
-  peptide prediction are biased due to the selection of negative 
-  data.*](https://doi.org/10.1101/2022.05.30.493946)
+Katarzyna Sidorczuk, Przemysław Gagat, Filip Pietluch, Jakub Kała, Dominik Rafacz, Laura Bąkała, Jadwiga Słowik, Rafał Kolenda, 
+Stefan Rödiger, Legana C H W Fingerhut, Ira R Cooke, Paweł Mackiewicz, Michał Burdukiewicz, Benchmarks in antimicrobial peptide 
+prediction are biased due to the selection of negative data, Briefings in Bioinformatics, 2022;, bbac343, 
+[https://doi.org/10.1093/bib/bbac343](https://doi.org/10.1093/bib/bbac343).
   
   "
 }
